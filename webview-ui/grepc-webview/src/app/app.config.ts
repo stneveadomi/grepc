a4a1a4149d10baeeb1067792705546bff451fe0e
Steven Neveadomi (PC) <stneveadomi@gmail.com>
2024-04-18 18:13:30 -0700

    Remove unused routes.

--- webview-ui/grepc-webview/src/app/app.config.ts
+++ webview-ui/grepc-webview/src/app/app.config.ts
@@ -1,9 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [provideAnimations()]
 };
