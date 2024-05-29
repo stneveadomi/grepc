611f6423c1c17bff7af4c7bd4fd78e3c7d74c32f
it1190 <stneveadomi@yahoo.com>
2024-05-28 20:01:10 -0600

    Update all spec to pass under Jest.

--- webview-ui/grepc-webview/src/app/rule/rule.component.spec.ts
+++ webview-ui/grepc-webview/src/app/rule/rule.component.spec.ts
@@ -1,19 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RuleComponent } from './rule.component';
+import { Rule } from '../../models/rule';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('RuleComponent', () => {
   let component: RuleComponent;
   let fixture: ComponentFixture<RuleComponent>;
+  let injectedRule: Rule = new Rule(
+    'title'
+  );
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RuleComponent]
+      imports: [RuleComponent, BrowserAnimationsModule]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(RuleComponent);
     component = fixture.componentInstance;
+    component.rule = injectedRule;
     fixture.detectChanges();
   });
 
