676ebe50561c882c7f98e485db0d1e6b3b5806b2
stneveadomi <stneveadomi@gmail.com>
2024-07-04 13:57:33 -0600

    Add helper reverseMap() method for LocationState

--- ext-src/rules/locationState.ts
+++ ext-src/rules/locationState.ts
@@ -3,3 +3,7 @@ export enum LocationState {
     LOCAL = "Workspace Rule - saved only to this workspace",
     GLOBAL = "Global Rule - saved across all workspaces"
 }
+
+export function reverseMap(location: LocationState) {
+    return location === LocationState.GLOBAL ? 'GLOBAL' : 'LOCAL';
+}
