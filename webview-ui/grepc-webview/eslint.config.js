70e1356924a0c2e5dbd068b887f84e874c4d4c5e
stneveadomi <stneveadomi@gmail.com>
2024-07-07 18:50:57 -0600

    Introduce prettier to SPA.

--- webview-ui/grepc-webview/eslint.config.js
+++ webview-ui/grepc-webview/eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const eslintConfigPrettier = require("eslint-config-prettier");
 
 module.exports = tseslint.config(
   {
@@ -11,6 +12,7 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      eslintConfigPrettier
     ],
     processor: angular.processInlineTemplates,
     rules: {
