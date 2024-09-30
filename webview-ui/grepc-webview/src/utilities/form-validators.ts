2565f824c297dadef681d050a6fd817c43676392
Steven Neveadomi (PC) <stneveadomi@gmail.com>
2024-09-30 07:20:13 -0700

    Restore text-decoration regex.

--- webview-ui/grepc-webview/src/utilities/form-validators.ts
+++ webview-ui/grepc-webview/src/utilities/form-validators.ts
@@ -46,8 +46,9 @@ export class CSSValidator {
             case 'font-style':
                 return /^(normal|italic|oblique|inherit)$/.test(propertyValue);
             case 'text-decoration':
-                property = 'text-decoration';
-                break;
+                return /^((none|underline|overline|line-through|blink)(\s+(solid|double|dotted|dashed|wavy))?(\s+\w+)?|inherit)$/.test(
+                    propertyValue,
+                );
             case 'cursor':
                 return /^(auto|default|none|context-menu|help|pointer|progress|wait|cell|crosshair|text|vertical-text|alias|copy|move|no-drop|not-allowed|grab|grabbing|all-scroll|col-resize|row-resize|n-resize|e-resize|s-resize|w-resize|ne-resize|nw-resize|se-resize|sw-resize|ew-resize|ns-resize|nesw-resize|nwse-resize|zoom-in|zoom-out|inherit|initial|unset)$/.test(
                     propertyValue,
